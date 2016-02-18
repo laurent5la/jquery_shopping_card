ed12bab7f3b56e9c6d3b1158bd87f630fb682eb7
Leena Tahilramani <ltahilramani@ltahilramani-mbp.local>
2016-02-18 10:02:21 -0800

    Changes to be committed: 	modified:   app/Http/Controllers/ProductsController.php 	modified:   public/css/app.css 	deleted:    resources/views/layout.blade.php 	modified:   resources/views/partials/header.blade.php 	modified:   resources/views/products.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,26 +3,29 @@
 		<div class="container-fluid">
 			<div class="row row-1">
 				<div class="pull-left">
-					<a href="http://www.dnb.com/"></a>
 				</div>
 				<div class="pull-right">
 					<ul class="list-inline">
-						<li><span class="icon phone"></span> Call Us  @if($agent->isMobile())<a href="tel:+18006492568"> (800) 649-2568 </a>  @else() (800) 649-2568 @endif</li>
-						<li><span class="icon chat"></span><a><script>getBoldchatLink("header")</script></a></li>
-						<li><a href="https://www.dandb.com/product/ecomm/login?execution=e1s1" target="_blank">Login</a></li>
+						<li><span class="glyphicon glyphicon-earphone"></span> Call Us</li>
+						<li><span class="glyphicon glyphicon-shopping-cart"></span> Cart</li>
+						<li><a href="" target="_blank"> Login</a></li>
 					</ul>
 				</div>
 			</div>
 			<div class="row row-2">
 				<div class="pull-left">
 					<ul class="list-inline">
-						<li><a href="/cos">Manage My Business Credit</a></li>
-						<li><a href="/coo">Business Credit Reports</a></li>
-						<li><a href="https://dnb.com/duns-number.html" target="_blank">D-U-N-S Number</a></li>
+						<li><a href="">Manage My Business Credit</a></li>
+						<li><a href="">Business Credit Reports</a></li>
+						<li><a href="" target="_blank">Number</a></li>
 					</ul>
 				</div>
 				<div class="pull-right">
-					<span class="toggle-search"><img src="/images/magnify-glass-icon.png"></span>
+					
+					<div class="form-group has-feedback">
+						<div class= "left-addon"><span class="  glyphicon glyphicon-search  form-control-feedback"></span></div>
+						<input type="text" class="form-control" placeholder="Search Business"/>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -39,17 +42,16 @@
 			<div class="navbar-toggle pull-right">
 				<span class="toggle-search"><img src="/images/magnify-glass-icon.png"></span>
 			</div>
-			<div class="logo navbar-nav"><a href="http://www.dnb.com/"></a></div>
+			<div class="logo navbar-nav"><a href=""></a></div>
 		</div>
 		<div id="mobile-menu" class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
 				<li class="toggle-menu text-right"><a><span class="glyphicon glyphicon-remove"></span></a></li>
-				<li><a href="/cos">Manage My Business Credit</a></li>
-				<li><a href="/coo">Business Credit Reports</a></li>
-				<li><a href="https://dnb.com/duns-number.html">D-U-N-S Number</a></li>
-				<li><a><script>getBoldchatLink("header")</script></a></li>
-				<li><a href="https://www.dandb.com/product/ecomm/login?execution=e1s1">Login</a></li>
-				<li><a href="http://www.dnb.com/company/contact-us.html">Contact Us</a></li>
+				<li><a href="">Manage My Business Credit</a></li>
+				<li><a href="">Business Credit Reports</a></li>
+				<li><a href=""> Number</a></li>
+				<li><a href="">Login</a></li>
+				<li><a href="">Contact Us</a></li>
 			</ul>
 		</div>
 	</nav>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,25 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" type="text/css" href="css/app.css"></link>
+<link rel="stylesheet" type="text/css" href="css/style.css"></link>
 	@include("partials.head")
 
 		<div id="main-container" class="container">
 
-		@include('partials.modal')
-		@yield('modal')
 
 		@include('partials.header')
 		@yield('header')
 
-		@include('partials.search')
-		@yield('search')
+		
 
 		<div id="content">
 			@yield('content')
 		</div>
 		
-		@include('partials.footer')
-		@yield('footer')
-	</div>
+		</div>
 
 	</body>
 </html>
