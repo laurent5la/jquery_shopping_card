0d07e237a100ee868451de965971caec9ae51905
Leena Tahilramani <ltahilramani@ltahilramani-mbp.local>
2016-02-22 17:12:40 -0800

    modified:   public/css/styles.css 	modified:   resources/views/partials/billingform.blade.php

--- resources/views/partials/billingform.blade.php
+++ resources/views/partials/billingform.blade.php
@@ -18,22 +18,22 @@
 	<label class="billingform__label" for="billingform">Billing Information</label>
 
 	<form role="form" id="billingform" class="form-group">
-		<div class="form-group">
+		<div id="billingform__input--div"class="form-group ">
 			<input type="text" name="cardname" placeholder="Name on card" class="form-control"/> 
 		</div>
-		<div class="form-group">
+		<div id="billingform__input--div" class="form-group">
 			<input type="text" name="ccnumber" id="ccnumber" placeholder="Credit card Number" class="form-control"/>
 		</div>
-		<div class="form-group">
+		<div id="billingform__input--div" class="form-group">
 			<input type="text" name="cvv" placeholder="CVV" class="form-control"/>
 		</div>
-		<div class="form-group">
+		<div id="billingform__input--div" class="form-group">
 			<input type="text" name="expdate" placeholder="Expiry date" class="form-control"/>
 		</div>
-		<div class="form-group">
+		<div id="billingform__input--div" class="form-group">
 			<input type="text" name="address" placeholder="Address" class="form-control"/>
 		</div>
-		<div class="form-group">
+		<div id="billingform__input--div" class="form-group">
 			<input type="text" name="phonenumber" placeholder="Phone Number" class="form-control"/>
 		</div>
 	</form>
