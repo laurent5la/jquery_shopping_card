02e4c38bc5afe5d623a30f6c869342f29af6d01c
parakhkunal <parakh.kunal@gmail.com>
2016-02-23 16:24:34 -0800

    changes for What's Included + Compare Products

--- resources/views/partials/compare_products.blade.php
+++ resources/views/partials/compare_products.blade.php
@@ -1,36 +1,40 @@
-<div class="compare_title_div">
-	<h3 class="compare_card_title">{{$product['name']}}</h3>
-</div>
-<div class="compare_option">
-	<div class="compare_price">
-		<h1>
-			@if(empty($product['quantity'][0]['price']['dollars']))
-				@if ($product["code"] == "cs")
-					<span class="text-uppercase">Free</span>
-				@else
-					Call Us<small>For a quote</small>
-				@endif
-			@else
-				<sup>$</sup>{{$product['quantity'][0]['price']['dollars']}}<sup>{{$product['quantity'][0]['price']['cents']}}</sup>@if(Session::get('crediton') == 'cos')<small>Per Month</small>@else<small>Per Report</small>@endif
-			@endif
-		</h1>
-	</div>
+@for ($i=0; $i < count($products)-1; $i++ )
+	<div class="col-md-4 hidden-xs hidden-sm compare_card" id="compare_option_{{$products[$i]['code']}}">
+		<div class="compare_title_div">
+			<h3 class="compare_card_title">{{$products[$i]['name']}}</h3>
+		</div>
+		<div class="compare_option">
+			<div class="compare_price">
+				<h1>
+					@if(empty($products[$i]['quantity'][0]['price']['dollars']))
+						@if ($products[$i]["code"] == "cs")
+							<span class="text-uppercase">Free</span>
+						@else
+							Call Us<small>For a quote</small>
+						@endif
+					@else
+						<sup>$</sup>{{$products[$i]['quantity'][0]['price']['dollars']}}<sup>{{$products[$i]['quantity'][0]['price']['cents']}}</sup>@if(Session::get('crediton') == 'cos')<small>Per Month</small>@else<small>Per Report</small>@endif
+					@endif
+				</h1>
+			</div>
 
-	<div class="border">
-		<button class="{{$product['code']}}" data-toggle="modal" data-target="#upgrade_modal_{{$product['code']}}">
-			{{$product['call_to_action']}}
-		</button>
-@if($product['code'] == 'cs' || $product['code'] == 'cep')
-		<h3>Includes:</h3>
-@else
-		<h3>Plus:</h3>
-@endif
-		<ul class="{{$product['code']}}">
-			@if(count($product['compare_bullets']) > 0)
-				@foreach($product['compare_bullets'] as $bullet)
-					<li><span>{{$bullet}}</span></li>
-				@endforeach
-			@endif
-		</ul>
+			<div class="border">
+				<button class="{{$products[$i]['code']}}" data-toggle="modal" data-target="#upgrade_modal_{{$products[$i]['code']}}">
+					{{$products[$i]['call_to_action']}}
+				</button>
+		@if($products[$i]['code'] == 'product1' || $products[$i]['code'] == 'product2')
+				<h3>Includes:</h3>
+		@else
+				<h3>Plus:</h3>
+		@endif
+				<ul class="{{$products[$i]['code']}}">
+					@if(count($products[$i]['compare_bullets']) > 0)
+						@foreach($products[$i]['compare_bullets'] as $bullet)
+							<li><span>{{$bullet}}</span></li>
+						@endforeach
+					@endif
+				</ul>
+			</div>
+		</div>
 	</div>
-</div>
+@endfor
--- resources/views/partials/included.blade.php
+++ resources/views/partials/included.blade.php
@@ -1,69 +1,88 @@
-<div class="col-md-4 col-md-push-8 product_selected {{$product['code']}}-bg">
-    <h1 class="visible-xs-* hidden-lg hidden-md">{{$product['name']}}</h1>
-    @if ($product['quantity'][0]['price']['dollars'])
-        <h1>${{$product['quantity'][0]['price']['dollars']}}@if ($product['quantity'][0]['price']['cents'])<sup>{{$product['quantity'][0]['price']['cents']}}</sup>@endif</h1>
-    @endif
-    <h3>{{$product['quantity'][0]['short_description']}}</h3>
-    <form class="{{$product['code']}}-bg">
-        <ul class="hollow-radios">
-            @if (count($product['quantity']))
-                @foreach ($product['quantity'] as $j=>$quantity)
-                    <li>
-                        <input type="radio"
-                               id="product_{{$product['code']}}_{{$quantity['product_id']}}"
-                               name="Product_{{$product['code']}}"
-                               value="{{$quantity['count']}}"
-                               data-product="{{$product['code']}}"
-                               data-upgrade="{{$products[($i+1)%count($products)]['code']}}"
-                               data-product_id="{{$quantity['product_id']}}"
-                               data-price_id="{{$quantity['price_id']}}"
-                               data-description="{{$quantity['short_description']}}"
-                               data-dollars="{{$quantity['price']['dollars']}}"
-                               data-cents="{{$quantity['price']['cents']}}"
-                               @if (!$j)
-                               checked="true"
-                                @endif
-                        >
-                        <label for="product_{{$product['code']}}_{{$quantity['product_id']}}">
-                            <span></span>
-                            <label>
-                                {{$quantity['count']}}
-                                @if ($quantity['count'] == 1)
-                                    company
-                                @else
-                                    companies
-                                @endif
-                                - ${{$quantity['price']['dollars']}}
-                                @if ($quantity['price']['cents'])
-                                    <sup class="cents">{{$quantity['price']['cents']}}</sup>
-                                @endif
-                            </label>
-                        @if ( floor($product['quantity'][0]['price']['dollars'] * $quantity['count']) > $quantity['price']['dollars'] )
-                        <label class="savings">Save {{floor(
-	                        	(
-	                        		1-
-                        			$quantity['price']['dollars'] / 
-                        			(
-                        				$product['quantity'][0]['price']['dollars'] * $quantity['count']
-                        			)
-								)*100
-                        	) }}%</label>
-                        @endif
-                        </label>
-                    </li>
-                @endforeach
-            @endif
-        </ul>
-    </form>
-    <!-- //was ($product['phone'] -->
-    @if ($product['phone'] != false)
-        <button class="{{$product['code']}} " onClick="window.location.href='tel:{{$products[$i]['phone']}}';return false;">
-            {{$product['call_to_action']}}
-        </button>
-    @else
-        <button class="{{$product['code']}} " data-toggle="modal" data-target="#upgrade_modal_{{$product['code']}}">
-            {{$product['call_to_action']}}
-        </button>
-    @endif
-</div>
+<div id="product_{{$products[0]['code']}}" class="row product {{$products[0]['code']}}-border-dark" style="display: inline-block;">
+  <div class="col-md-4 col-md-push-8 product_selected {{$products[0]['code']}}-bg">
+      <h1 class="visible-xs-* hidden-lg hidden-md">{{$products[0]['name']}}</h1>
+      @if ($products[0]['quantity'][0]['price']['dollars'])
+          <h1><sup>$</sup>{{$products[0]['quantity'][0]['price']['dollars']}}@if ($products[0]['quantity'][0]['price']['cents'])<sup>{{$products[0]['quantity'][0]['price']['cents']}}</sup>@if(Session::get('crediton') == 'cos')<small>/mo</small>@endif @endif
+          </h1>
+      @elseif ($products[0]['quantity'][0]['price']['dollars'] === 0 && $products[0]['quantity'][0]['price']['cents'] === 0)
+          <h1>Free</h1>
+      @elseif (is_null($products[0]['quantity'][0]['price']['dollars']) && is_null($products[0]['quantity'][0]['price']['cents']))
+          <h1>Call Us</h1>
+          <h3>or</h3>
+      @endif
+      <h3>{{$products[0]['quantity'][0]['short_description']}}</h3>
+      <form class="{{$products[0]['code']}}-bg">
+          <ul class="hollow-radios">
+              @if (count($products[0]['quantity']) > 1)
+                  @foreach ($products[0]['quantity'] as $j=>$quantity)
+                      <li>
+                          <input type="radio"
+                                 class="product_quantity"
+                                 id="product_{{$products[0]['code']}}_{{$quantity['product_id']}}"
+                                 name="Product_{{$products[0]['code']}}"
+                                 value="{{$quantity['count']}}"
+                                 data-product="{{$products[0]['code']}}"
+                                 data-product_id="{{$quantity['product_id']}}"
+                                 data-price_id="{{$quantity['price_id']}}"
+                                 data-description="{{$quantity['short_description']}}"
+                                 data-dollars="{{$quantity['price']['dollars']}}"
+                                 data-cents="{{$quantity['price']['cents']}}"
+                                 onChange = "appendPrice();"
+                                 @if (!$j)
+                                 checked="true"
+                                  @endif
+                          >
 
+                          <label for="product_{{$products[0]['code']}}_{{$quantity['product_id']}}">
+                              <span></span>
+                              <label>
+                                  {{$quantity['count']}}
+                                  @if ($quantity['count'] == 1)
+                                      company
+                                  @else
+                                      companies
+                                  @endif
+                                  - ${{$quantity['price']['dollars']}}
+                                  @if ($quantity['price']['cents'])
+                                      <sup class="cents">{{$quantity['price']['cents']}}</sup>
+                                  @endif
+                              </label>
+                          @if ( floor($products[0]['quantity'][0]['price']['dollars'] * $quantity['count']) > $quantity['price']['dollars'] )
+                          <label class="savings">Save {{floor(
+                              (
+                                1-
+                                $quantity['price']['dollars'] /
+                                (
+                                  $products[0]['quantity'][0]['price']['dollars'] * $quantity['count']
+                                )
+                  )*100
+                            ) }}%</label>
+                          @endif
+                      </label>
+                  </li>
+                  <input type="hidden" id="quantity" value="{{$quantity['count']}}">
+                  <input type="hidden" id="price_dollars_{{$quantity['count']}}" value="{{$quantity['price']['dollars']}}">
+                  <input type="hidden" id="price_cents_{{$quantity['count']}}" value="{{$quantity['price']['cents']}}">
+                  @endforeach
+              @endif
+          </ul>
+      </form>
+      <button type="button" onClick = "appendPrice();" class="{{$products[0]['code']}}" id="{{$products[0]['code']}}" class="btn btn-info btn-lg" data-id="{{$quantity['count']}}" data-toggle="modal" data-target="#upgrade_modal_{{$products[0]['code']}}">{{$products[0]['call_to_action']}}</button>
+  </div>
+  <div class="col-md-8 col-md-pull-4 product_included_description">
+    <div class="col-md-6">
+      <ul class="product1">
+        @for ($k=0; $k < count($products[0]['included'])/2; $k++ )
+          <li class = "product1-li-checks"><span>{{$products[0]['included'][$k]}}</span></li>
+        @endfor
+      </ul>
+    </div>
+    <div class="col-md-6">
+      <ul class="product1">
+        @for ($k=count($products[0]['included'])/2 + 1; $k < count($products[0]['included']); $k++ )
+          <li class = "product1-li-checks"><span>{{$products[0]['included'][$k]}}</span></li>
+        @endfor
+      </ul>
+    </div>
+    </div>
+</div>
