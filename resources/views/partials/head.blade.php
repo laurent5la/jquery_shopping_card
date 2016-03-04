97f2dcb9a953ecd9eb853849fa967de56cfa0b9e
Leena Tahilramani <ltahilramani@dandb.com>
2016-03-04 10:48:24 -0800

    Adding head and login back

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,43 @@
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="robots" content="noindex, nofollow">
+
+    <title>{{$page_title or 'Company Search'}}</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css"></link>
+    <link rel="stylesheet" type="text/css" href="css/style.css"></link>
+    <link rel="stylesheet" type="text/css" href="css/styles.css"></link>
+    <link rel="stylesheet" type="text/css" href="css/login-register.css"></link>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/4.12.0/bootstrap-social.css"></link>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <!-- Jquery expansion -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="js/app.js"></script>
+    <link href="css/bootstrap-formhelpers.min.css" rel="stylesheet">
+    
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/4.12.0/bootstrap-social.css"></link>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.7/jquery.validate.min.js"></script>
+    <script src="js/custom_plugin.js"></script>
+    <script src="js/pwstrength-bootstrap-1.2.9.js"></script>
+    <script src="js/q.js"></script>
+    <script src="js/bootstrap-formhelpers.min.js"></script>
+
+    <script src="js/validation.js"></script>
+    <script src="js/cybersource.js"></script>
+
+    <script src="js/global.js"></script>
+    <script src="js/chooseForm.js"></script>
+    <script src="js/login.js"></script>
+    <script src="js/billing.js"></script>
+    <script src="js/register.js"></script>
+    <script src="js/personalInformation.js"></script>
---
+++ resources/views/partials/loginform.blade.php
@@ -0,0 +1,71 @@
+<div id="loginform-div">
+	<div id="invalidUser-div" class="error" style="display:none;"></div>
+<<<<<<< Updated upstream
+	{{--<label class="login-form__label" for="loginform">Create Account or <span class="login-form__login-text">Login</span></label>--}}
+	<div class="CREATE_OR_LOGIN title"><span class="create">Create Account</span> or <span class="login clickable">Login</span></div>
+
+
+	<form class=" LOGGED_IN form-group"id="loginform" role="form">
+		<div class="form-group">
+				<span class="login-icon inner-addon left-addon">
+					<i class="  glyphicon glyphicon-user"></i>
+				</span>
+			<input class=""id="username" type="text" name="username" placeholder="Username">
+		</div>
+		<br>
+		<div class="form-group NEW_USER">
+
+=======
+		{{--<label class="login-form__label" for="loginform">Create Account or <span class="login-form__login-text">Login</span></label>--}}
+		<div class="CREATE_OR_LOGIN title"><span class="create">Create Account</span> or <span class="login clickable">Login</span></div>
+
+
+	<form class=" LOGGED_IN form-group"id="loginform" role="form">
+			<div class="form-group">
+				<span class="login-icon inner-addon left-addon">
+					<i class="  glyphicon glyphicon-user"></i>
+				</span>
+				<input class=""id="username" type="text" name="username" placeholder="Username">
+			</div>
+			<br>
+			<div class="form-group NEW_USER">
+>>>>>>> Stashed changes
+				<span class="login-icon inner-addon left-addon">
+					<i class="glyphicon glyphicon-envelope"></i>
+				</span>
+			<input id="email" type="text" name="email" placeholder="Email"/>
+		</div>
+		<br>
+		<div class="form-group" id="pwd-container">
+				<span class="login-icon inner-addon left-addon">
+					<i class="  glyphicon glyphicon-lock"></i>
+				</span>
+<<<<<<< Updated upstream
+			<input type="password"  id="password1" name="password1" placeholder="Password">
+			<div style="visibility: hidden;" class="pwstrength_viewport_progress"></div>
+		</div>
+		<div class="form-group NEW_USER">
+				<span class="login-icon inner-addon left-addon">
+					<i class="  glyphicon glyphicon-lock"></i>
+				</span>
+			<input id="cpassword" type="password" name="cpassword"  placeholder="Password Confirmation"/>
+		</div>
+	</form>
+</div>
+=======
+				<input type="password"  id="password1" name="password1" placeholder="Password">
+				<div style="visibility: hidden;" class="pwstrength_viewport_progress"></div>
+			</div>
+			<div class="form-group NEW_USER">
+				<span class="login-icon inner-addon left-addon">
+					<i class="  glyphicon glyphicon-lock"></i>
+				</span>
+				<input id="cpassword" type="password" name="cpassword"  placeholder="Password Confirmation"/>
+			</div>
+ 		</form>
+
+
+
+
+>>>>>>> Stashed changes
+</div>
